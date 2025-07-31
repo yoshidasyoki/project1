943eee84baf41e573fe07e00b60fcb69f7e3c0f0
yoshidasyoki <118053871+yoshidasyoki@users.noreply.github.com>
2025-07-31 20:50:49 +0900

    Modified file(setData.php).

--- src/src/setData.php
+++ src/src/setData.php
@@ -4,14 +4,17 @@ function setData(PDO $dbh): void
 {
     # ログファイルに関する情報取得
     $fileCount = count(glob("log_files/*"));
-    $fileName = basename(glob("log_files/*")[0]);
-    # エスケープ処理
-    if (!preg_match('/^[a-zA-Z0-9._-]+$/', $fileName)) {
-        throw new Exception('無効な文字列が使用されています。ログファイル名を変更してください。');
-    }
 
     # ログファイルが1件だけ存在する場合データをインポート
     if ($fileCount === 1) {
+        # ログファイル名の取得
+        $fileName = basename(glob("log_files/*")[0]);
+
+        # エスケープ処理
+        if (!preg_match('/^[a-zA-Z0-9._-]+$/', $fileName)) {
+            throw new Exception('無効な文字列が使用されています。ログファイル名を変更してください。');
+        }
+
         $sql = <<<SQL
             LOAD DATA INFILE '/var/lib/mysql-files/{$fileName}'
             INTO TABLE wiki_log
